7483659239641cb3178c8cb51947e56c754570b6
Cherry-Syrina <sushma.shukla3011@gmail.com>
2026-06-06 17:14:01 +0530

    Initial commit

---
+++ src/components/footer.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+def footer_home():
+
+    st.markdown("""
+        <div style="text-align:center;">
+            <p style="font-weight:bold; color:white;">
+                Created with ❤️ by Sushma Shukla
+            </p>
+        </div>
+    """, unsafe_allow_html=True)
+
+
+def footer_dashboard():
+
+    st.markdown("""
+        <div style="text-align:center;">
+            <p style="font-weight:bold; color:black;">
+                Created with ❤️ by Sushma Shukla
+            </p>
+        </div>
+    """, unsafe_allow_html=True)
